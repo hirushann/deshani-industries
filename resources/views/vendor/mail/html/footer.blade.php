3774666112e216f1a682eb981f96587735185605
Hirushan Perera <hirushanperera@Hirushans-MacBook-Air.local>
2026-08-20 12:27:00 +0530

    invoice

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,16 @@
+<tr>
+<td>
+<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td class="content-cell" align="center">
+{{ Illuminate\Mail\Markdown::parse($slot) }}
+<p style="margin-top: 15px; margin-bottom: 0; color: #a1a1aa; font-size: 13px; line-height: 1.6; text-align: center;">
+    <strong>{{ config('app.name', 'Deshani Industries') }}</strong><br>
+    101/1 Bogahawatta road, Hekitta, Wattala<br>
+    Phone: <a href="tel:0777386356" style="color: #a1a1aa; text-decoration: none;">0777386356</a>
+</p>
+</td>
+</tr>
+</table>
+</td>
+</tr>
